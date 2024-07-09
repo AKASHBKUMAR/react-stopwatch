b587b2b3179ca80a5e25fcf052981e45dd0496e4
Akash_Kumar <akashbkumar30@gmail.com>
2024-07-09 14:48:09 +0530

    Final React Stopwatch

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderComponent from './Components/HeaderComponent'
+
+const App = () => {
+  return (
+    <>
+      <HeaderComponent />
+    </>
+  )
+}
+
+export default App
